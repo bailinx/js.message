bedc0e170419ac727596a8f81267c3d8f2a1e73e
radishj <423261989@qq.com>
2015-11-30 23:32:08 +0800

    增加confirm并增加回调方法

--- README.md
+++ README.md
@@ -13,14 +13,22 @@
 window.alert = function(msg) {
     var message = new jMessage();
     message.alert(msg);
+    /*message.alert(msg, function() {
+        console.log('callback! - ' + (new Date()).valueOf());
+    });*/
 }
 function funAlert() {
     alert("操作已成功");
 }
 ```
 ## 已知问题
-* 组件无法阻塞页面(若用Interval模拟，太消耗内存，得不偿失，后续考虑加入回调方法)
+* ~~组件无法阻塞页面(若用Interval模拟，太消耗内存，得不偿失，后续考虑加入回调方法)~~
 
 ## 更新日志
+#### Message v1.0.1 -2015/11/30
+* 新增alert回调方法
+* 完善confirm部分
+* 考虑合并dialog dom生成(太多相似)
+
 #### Message v1.0.0 - 2015/11/25
 * 创建项目及完善alert逻辑
--- js/message.js
+++ js/message.js
@@ -37,7 +37,7 @@ Message.prototype.showMask = function () {
  * 窗体是否存在
  * @return {[type]}
  */
-Message.prototype.existAlertCnt = function () {
+Message.prototype.existDialog = function () {
     var _dialog = document.getElementsByClassName('ui-dialog');
     if( !!_dialog && !_dialog.length ) {
         return false;
@@ -46,13 +46,14 @@ Message.prototype.existAlertCnt = function () {
     }
 }
 /**
- * 显示窗体
- * @param  {[type]} msg 消息
- * @return {[type]}     [description]
+ * 显示Alert窗体
+ * @param  {[type]} msg      消息
+ * @param  {[type]} callBack 回调方法
+ * @return {[type]}          [description]
  */
-Message.prototype.showAlertCnt = function (msg) {
+Message.prototype.showAlertCnt = function (msg, callBack) {
     var _self = this;
-    if( !_self.existAlertCnt() ) {
+    if( !_self.existDialog() ) {
         // <section class="ui-dialog">
         var _dialog = document.createElement('section');
         _dialog.className = "ui-dialog";
@@ -99,13 +100,131 @@ Message.prototype.showAlertCnt = function (msg) {
         if(_btnSure.addEventListener){
             _btnSure.addEventListener("click",function() {
                 _self.hide();
+                // 回调方法
+                if( typeof callBack == 'function' ) {
+                    callBack();
+                }
             },false);
         } else if(_btnSure.attachEvent){
             _btnSure.attachEvent("onclick",function() {
                 _self.hide();
+                // 回调方法
+                if( typeof callBack == 'function' ) {
+                    callBack();
+                }
             });
         }
         _dialogFooter.appendChild(_btnSure);
+
+        _dialogBody.appendChild(_dialogTitle);
+        _dialogBody.appendChild(_dialogContent);
+        _dialogBody.appendChild(_dialogFooter);
+
+        // 组装
+        _dialogCnt.appendChild(_dialogClose);
+        _dialogCnt.appendChild(_dialogBody);
+        _dialog.appendChild(_dialogCnt);
+        //_dialog.style.display = "none";
+
+        document.body.appendChild(_dialog);
+
+    } else {
+        var _dialog = document.getElementsByClassName('ui-dialog');
+        var _dialogTitle = _dialog[0].getElementsByClassName("ui-dialog-title");
+        _dialogTitle[0].innerText = msg;
+
+        _dialog[0].style.display = "block";
+    }
+}
+/**
+ * 显示Confirm窗体<未完成.>
+ * @param  {[type]} msg      消息
+ * @param  {[type]} callBack 回调方法
+ * @return {[type]}          [description]
+ */
+Message.prototype.showConfirmCnt = function (msg, callBack) {
+    var _self = this;
+    if( !_self.existDialog() ) {
+        // <section class="ui-dialog">
+        var _dialog = document.createElement('section');
+        _dialog.className = "ui-dialog";
+        //   <div class="ui-dialog-cnt">
+        var _dialogCnt =  document.createElement('div');
+        _dialogCnt.className = "ui-dialog-cnt";
+        //      <span class="ui-dialog-close"></span>
+        var _dialogClose = document.createElement('span');
+        _dialogClose.className = "ui-dialog-close";
+        if(_dialogClose.addEventListener){
+            _dialogClose.addEventListener("click",function() {
+                _self.hide();
+            },false);
+        } else if(_dialogClose.attachEvent){
+            _dialogClose.attachEvent("onclick",function() {
+                _self.hide();
+            });
+        }
+
+        //      <div class="ui-dialog-bd">
+        var _dialogBody = document.createElement('div');
+        //          <h3 class="ui-dialog-title">
+        var _dialogTitle = document.createElement('h3');
+        _dialogTitle.className = "ui-dialog-title";
+        _dialogTitle.innerText = msg;
+        //          <div class="ui-dialog-content">
+        var _dialogContent = document.createElement('div');
+        _dialogContent.className = "ui-dialog-content";
+        //              <div class="ui-dialog-content-in"></div>
+        var _dialogContentIn = document.createElement('div');
+        _dialogContentIn.className = "ui-dialog-content-in";
+        _dialogContent.appendChild(_dialogContentIn);
+
+        //          <div class="ui-dialog-ft">
+        var _dialogFooter = document.createElement('div');
+        _dialogFooter.className = "ui-dialog-ft";
+        //              <span class="ui-btn-adapt ui-btn-adapt-basic-s">
+        var _btnSure = document.createElement('div');
+        _btnSure.className = "ui-btn-adapt ui-btn-adapt-basic-s";
+        //                  <span>
+        var _btnSureMsg = document.createElement('span');
+        _btnSureMsg.innerText = "确定";
+        _btnSure.appendChild(_btnSureMsg);
+        //              <span class="ui-btn-adapt ui-btn-adapt-basic-c">
+        var _btnCancle = document.createElement('div');
+        _btnCancle.className = "ui-btn-adapt ui-btn-adapt-basic-c";
+        var _btnCancleMsg = document.createElement('span');
+        _btnCancleMsg.innerText = "取消";
+        _btnCancle.appendChild(_btnCancleMsg);
+        // sure
+        if(_btnSure.addEventListener){
+            _btnSure.addEventListener("click",function() {
+                _self.hide();
+                // 回调方法
+                if( typeof callBack == 'function' ) {
+                    callBack();
+                }
+            },false);
+        } else if(_btnSure.attachEvent){
+            _btnSure.attachEvent("onclick",function() {
+                _self.hide();
+                // 回调方法
+                if( typeof callBack == 'function' ) {
+                    callBack();
+                }
+            });
+        }
+        // cancle
+        if(_btnCancle.addEventListener){
+            _btnCancle.addEventListener("click",function() {
+                _self.hide();
+            },false);
+        } else if(_btnCancle.attachEvent){
+            _btnCancle.attachEvent("onclick",function() {
+                _self.hide();
+            });
+        }
+
+        _dialogFooter.appendChild(_btnSure);
+        _dialogFooter.appendChild(_btnCancle);
 
         _dialogBody.appendChild(_dialogTitle);
         _dialogBody.appendChild(_dialogContent);
@@ -133,19 +252,30 @@ Message.prototype.showAlertCnt = function (msg) {
  */
 Message.prototype.hide = function () {
     var _self = this;
-    if( _self.existAlertCnt() && _self.existMask()) {
+    if( _self.existDialog() && _self.existMask()) {
         document.getElementsByClassName('ui-dialog')[0].style.display = "none";
         document.getElementsByClassName('ui-dialog-mask')[0].style.display = "none";
     }
 }
 /**
- * 弹出窗体方法
+ * 弹出Alert窗体方法
  * @param  {[type]} msg [description]
  * @return {[type]}     [description]
  */
-Message.prototype.alert = function (msg) {
+Message.prototype.alert = function (msg, callBack) {
     var _self = this;
-
+    _self.hide();
     _self.showMask();
-    _self.showAlertCnt(msg);
+    _self.showAlertCnt(msg, callBack);
+}
+/**
+ * 弹出Confirm窗体方法
+ * @param  {[type]} msg [description]
+ * @return {[type]}     [description]
+ */
+Message.prototype.confirm = function (msg, callBack) {
+    var _self = this;
+    _self.hide();
+    _self.showMask();
+    _self.showConfirmCnt(msg, callBack);
 }
