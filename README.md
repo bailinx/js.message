7723bef86754d02d8221ff69c6d17f4ab898d034
radishj <423261989@qq.com>
2015-12-02 13:31:17 +0800

    完善内部事件绑定方法，及处理逻辑

--- README.md
+++ README.md
@@ -10,21 +10,39 @@
 ```
 初始化方法如下：
 ```javascript
-window.alert = function(msg) {
-    var message = new jMessage();
-    message.alert(msg);
-    /*message.alert(msg, function() {
-        console.log('callback! - ' + (new Date()).valueOf());
-    });*/
+window.alert = function(msg, callBack) {
+    var message = new Message();
+    message.alert(msg, callBack);
+}
+window.confirm = function(msg, callBack) {
+    var message = new Message();
+    message.confirm(msg, callBack);
 }
 function funAlert() {
-    alert("操作已成功");
+    alert("操作已成功", function() {
+        console.log('操作已成功');
+    });
+}
+function funConfirm() {
+    confirm("操作已成功", function(result) {
+        if(result) {
+            console.log('点击确定! - ' + '结果:' + result + (new Date()).valueOf());
+        } else {
+            console.log('点击取消! - ' + '结果:' + result + (new Date()).valueOf());
+        }
+    });
 }
 ```
 ## 已知问题
-* ~~组件无法阻塞页面(若用Interval模拟，太消耗内存，得不偿失，后续考虑加入回调方法)~~
+* ~~组件无法阻塞页面(若用Interval模拟，太消耗内存，得不偿失，后续考虑加入回调方法)(v1.0.0)~~
+* ~~组件中(匿名)事件重复绑定问题(v1.0.1)~~
 
 ## 更新日志
+#### Message v1.0.2 -2015/12/02
+* 完善内部事件处理方法
+* 修改完善逻辑
+* 修改遗留Bug
+
 #### Message v1.0.1 -2015/11/30
 * 新增alert回调方法
 * 完善confirm部分
--- js/message.js
+++ js/message.js
@@ -3,7 +3,7 @@
  * radishj - v1.0.0 (2015-11-25 14:03:58)
  * https://github.com/RadishJ | Released under MIT license
  */
-var Message = function () {
+var Message = function() {
     "use strict";
 }
 /**
@@ -11,25 +11,25 @@ var Message = function () {
  * @type {Object}
  */
 Message.prototype.TYPE = {
-    ALERT   : 0,
-    CONFIRM : 1
+    ALERT: 0,
+    CONFIRM: 1
 }
 /**
  * 窗体图标
  * @type {Object}
  */
 Message.prototype.ICON = {
-    NONE    : 0,
-    FAILED  : 1,
-    SUCCESS : 2
+    NONE: 0,
+    FAILED: 1,
+    SUCCESS: 2
 }
 /**
  * 遮罩层是否存在
  * @return {[type]} [description]
  */
-Message.prototype.existMask = function () {
+Message.prototype.existMask = function() {
     var _mask = document.getElementsByClassName('ui-dialog-mask');
-    if( !!_mask && !_mask.length ) {
+    if (!!_mask && !_mask.length) {
         return false;
     } else {
         return true;
@@ -39,9 +39,9 @@ Message.prototype.existMask = function () {
  * 显示遮罩层
  * @return {[type]} [description]
  */
-Message.prototype.showMask = function () {
+Message.prototype.showMask = function() {
     var _self = this;
-    if( !_self.existMask() ) {
+    if (!_self.existMask()) {
         var _mask = document.createElement('div');
         _mask.className = "ui-dialog-mask";
         document.body.appendChild(_mask);
@@ -54,9 +54,9 @@ Message.prototype.showMask = function () {
  * 窗体是否存在
  * @return {[type]}
  */
-Message.prototype.existDialog = function () {
+Message.prototype.existDialog = function() {
     var _dialog = document.getElementsByClassName('ui-dialog');
-    if( !!_dialog && !_dialog.length ) {
+    if (!!_dialog && !_dialog.length) {
         return false;
     } else {
         return true;
@@ -68,21 +68,22 @@ Message.prototype.existDialog = function () {
  * @param  {[type]} callBack 回调方法
  * @return {[type]}          [description]
  */
-Message.prototype.showDialog = function (msg, type, icon, callBack) {
+Message.prototype.showDialog = function(msg, type, icon, callBack) {
     var _self = this;
-    if( !_self.existDialog() ) {
+    if (!_self.existDialog()) {
         // <section class="ui-dialog">
         var _dialog = document.createElement('section');
         _dialog.className = "ui-dialog";
         //   <div class="ui-dialog-cnt">
-        var _dialogCnt =  document.createElement('div');
+        var _dialogCnt = document.createElement('div');
         _dialogCnt.className = "ui-dialog-cnt";
         //      <span class="ui-dialog-close"></span>
         var _dialogClose = document.createElement('span');
         _dialogClose.className = "ui-dialog-close";
-        _self.addEvent(_dialogClose, 'click', function func() {
+        _self.base.addEvent(_dialogClose, 'click', function() {
             _self.hide();
         });
+        // 尝试过原生绑定事件，单匿名事件解绑始终是个问题
         /*if(_dialogClose.addEventListener){
             _dialogClose.addEventListener("click",function() {
                 _self.hide();
@@ -128,25 +129,8 @@ Message.prototype.showDialog = function (msg, type, icon, callBack) {
         var _btnSureMsg = document.createElement('span');
         _btnSureMsg.innerText = "确定";
         _btnSure.appendChild(_btnSureMsg);
-        // sure
-        /*if(_btnSure.addEventListener){
-            _btnSure.addEventListener("click", function() {
-                _self.hide();
-                // 回调方法
-                if( typeof callBack == 'function' ) {
-                    callBack(true);
-                }
-            },false);
-        } else if(_btnSure.attachEvent){
-            _btnSure.attachEvent("onclick", function() {
-                _self.hide();
-                // 回调方法
-                if( typeof callBack == 'function' ) {
-                    callBack(true);
-                }
-            });
-        }*/
-        _self.addEvent(_btnSure, 'click', function func() {
+        // sure event
+        _self.base.addEvent(_btnSure, 'click', function (){
             _self.hide();
             callBack(true);
         });
@@ -156,28 +140,15 @@ Message.prototype.showDialog = function (msg, type, icon, callBack) {
         var _btnCancleMsg = document.createElement('span');
         _btnCancleMsg.innerText = "取消";
         _btnCancle.appendChild(_btnCancleMsg);
-        // cancle
-        /*if(_btnCancle.addEventListener){
-            _btnCancle.addEventListener("click",function() {
+        // cancle event
+        if (_self.TYPE.CONFIRM === type) {
+            // 若alert窗体，无需绑定cancle按钮，否则造成事件重复绑定
+            _self.base.addEvent(_btnCancle, 'click', function() {
                 _self.hide();
-                // 回调方法
-                if( typeof callBack == 'function' ) {
-                    callBack(false);
-                }
-            },false);
-        } else if(_btnCancle.attachEvent){
-            _btnCancle.attachEvent("onclick",function() {
-                _self.hide();
-                // 回调方法
-                if( typeof callBack == 'function' ) {
-                    callBack(false);
-                }
+                callBack(false);
             });
-        }*/
-        _self.addEvent(_btnCancle, 'click', function func() {
-            _self.hide();
-            callBack(false);
-        });
+        }
+
         if (_self.TYPE.CONFIRM === type) {
             _btnSure.style.display = "inline-block";
             _btnCancle.style.display = "inline-block";
@@ -203,6 +174,7 @@ Message.prototype.showDialog = function (msg, type, icon, callBack) {
         document.body.appendChild(_dialog);
 
     } else {
+        // 没必要重绘窗体
         var _dialog = document.getElementsByClassName('ui-dialog');
         var _dialogTitle = _dialog[0].getElementsByClassName("ui-dialog-title");
         _dialogTitle[0].innerText = msg;
@@ -230,28 +202,18 @@ Message.prototype.showDialog = function (msg, type, icon, callBack) {
             // 重置图标
             _dialogIcon[0].style.display = "none";
         }
+        // 移除事件(试过自解绑匿名函数方法，然而后果是导致未触发的按钮的事件被重复绑定)
+        _self.base.removeEvent(_btnSure[0], 'click');
+        _self.base.removeEvent(_btnCancle[0], 'click');
+        _self.base.addEvent(_btnSure[0], 'click', function() {
+            _self.hide();
+            callBack(true);
+        });
+        _self.base.addEvent(_btnCancle[0], 'click', function() {
+            _self.hide();
+            callBack(false);
+        });
 
-        // 重置事件
-        // sure
-        /*if(_btnSure[0].addEventListener){
-            _btnSure[0].addEventListener("click", function() {
-                _self.hide();
-                // 回调方法
-                if( typeof callBack == 'function' ) {
-                    callBack(true);
-                }
-            },false);
-        } else if(_btnSure[0].attachEvent){
-            _btnSure[0].attachEvent("onclick", function() {
-                _self.hide();
-                // 回调方法
-                if( typeof callBack == 'function' ) {
-                    callBack(true);
-                }
-            });
-        }*/
-        _self.removeEvent(_btnSure[0], 'click', 'func');
-        _self.removeEvent(_btnCancle[0], 'click', 'func');
         _dialog[0].style.display = "block";
     }
 }
@@ -259,9 +221,9 @@ Message.prototype.showDialog = function (msg, type, icon, callBack) {
  * 隐藏窗体
  * @return {[type]} [description]
  */
-Message.prototype.hide = function () {
+Message.prototype.hide = function() {
     var _self = this;
-    if( _self.existDialog() && _self.existMask()) {
+    if (_self.existDialog() && _self.existMask()) {
         document.getElementsByClassName('ui-dialog')[0].style.display = "none";
         document.getElementsByClassName('ui-dialog-mask')[0].style.display = "none";
     }
@@ -271,7 +233,7 @@ Message.prototype.hide = function () {
  * @param  {[type]} msg [description]
  * @return {[type]}     [description]
  */
-Message.prototype.alert = function (msg, callBack) {
+Message.prototype.alert = function(msg, callBack) {
     var _self = this;
     _self.hide();
     _self.showMask();
@@ -282,63 +244,73 @@ Message.prototype.alert = function (msg, callBack) {
  * @param  {[type]} msg [description]
  * @return {[type]}     [description]
  */
-Message.prototype.confirm = function (msg, callBack) {
+Message.prototype.confirm = function(msg, callBack) {
     var _self = this;
     _self.hide();
     _self.showMask();
     _self.showDialog(msg, _self.TYPE.CONFIRM, _self.ICON.SUCCESS, callBack);
 }
-
-Message.prototype.addEvent = function(element, type, handler) {
-    var _self = this;
-    if (element.addEventListener) {
-        element.addEventListener(type, handler, false);
-    } else {
+/**
+ * Base方法
+ * @description 参考Dean Edwards大神绑定事件处理方法，
+ *              实现匿名方法绑定与解绑
+ *              http://dean.edwards.name/weblog/2005/10/add-event/
+ * @type {Object}
+ */
+Message.prototype.base = {
+    addEvent: function(element, type, handler) {
         // assign each event handler a unique ID
-        if (!handler.$$guid) handler.$$guid = _self.addEvent.guid++;
+        if (!handler.$$guid) handler.$$guid = this.addEvent.guid++;
+        // assign each element a unique ID
+        if (!element.$$guid) element.$$guid = this.addEvent.guid++;
         // create a hash table of event types for the element
-        if (!element.events) element.events = {};
+        if (!this.addEvent.handlers[element.$$guid]) this.addEvent.handlers[element.$$guid] = {};
         // create a hash table of event handlers for each element/event pair
-        var handlers = element.events[type];
+        var handlers = this.addEvent.handlers[element.$$guid][type];
         if (!handlers) {
-            handlers = element.events[type] = {};
-            // store the existing event handler (if there is one)
-            if (element["on" + type]) {
+            handlers = this.addEvent.handlers[element.$$guid][type] = {};
+            // store the existing event handler(if there is one)
+            /*if (element["on" + type]) {
                 handlers[0] = element["on" + type];
-            }
+            }*/
         }
         // store the event handler in the hash table
         handlers[handler.$$guid] = handler;
         // assign a global event handler to do all the work
-        element["on" + type] = handleEvent;
+        element["on" + type] = this.handleEvent;
+    },
+
+
+    removeEvent: function(element, type, handler) {
+        // check if the element has a guid
+        if (!element.$$guid) return;
+        if (handler) {
+            // 删除事件，如果在hash表
+            if (Message.prototype.base.addEvent.handlers[element.$$guid] && Message.prototype.base.addEvent.handlers[element.$$guid][type]) {
+                delete Message.prototype.base.addEvent.handlers[element.$$guid][type][handler.$$guid];
+            }
+        } else {
+            // 如果handler为undefined，则删除该类型全部事件
+            if (Message.prototype.base.addEvent.handlers[element.$$guid] && Message.prototype.base.addEvent.handlers[element.$$guid][type]) {
+                delete Message.prototype.base.addEvent.handlers[element.$$guid][type];
+            }
+        }
+
+    },
+    handleEvent: function(event) {
+        // grab the event object (IE uses a global event object)
+        event = event || window.event;
+        // get a reference to the hash table of event handlers
+        var handlers = Message.prototype.base.addEvent.handlers[this.$$guid][event.type];
+        // execute each event handler
+        for (var i in handlers) {
+
+            this.$$handleEvent = handlers[i];
+            this.$$handleEvent(event);
+        }
     }
-};
+}
 // a counter used to create unique IDs
-Message.prototype.addEvent.guid = 1;
-
-Message.prototype.removeEvent = function(element, type, handler) {
-    if (element.removeEventListener) {
-        element.removeEventListener(type, handler, false);
-    } else {
-        // delete the event handler from the hash table
-        if (element.events && element.events[type]) {
-            delete element.events[type][handler.$$guid];
-        }
-    }
-};
-
-Message.prototype.handleEvent = function(event) {
-    var returnValue = true;
-    // grab the event object (IE uses a global event object)
-    event = event || window.event;
-    // get a reference to the hash table of event handlers
-    var handlers = this.events[event.type];
-    // execute each event handler
-    for (var i in handlers) {
-        this.$$handleEvent = handlers[i];
-        if (this.$$handleEvent(event) === false) {
-            returnValue = false;
-        }
-    }
-    return returnValue;
-};
+Message.prototype.base.addEvent.guid = 1;
+// a global hash table containing all handlers
+Message.prototype.base.addEvent.handlers = {};
